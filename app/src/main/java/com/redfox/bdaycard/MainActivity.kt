17e2646e9489b66ca8a1eafeeabbc964b3f7186f
debz-g <debz.exe@gmail.com>
2021-09-12 01:16:33 +0530

    - configured lottie animations

--- app/src/main/java/com/redfox/bdaycard/MainActivity.kt
+++ app/src/main/java/com/redfox/bdaycard/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.redfox.bdaycard
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.redfox.bdaycard.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_main)
+        setupAnim()
+    }
 
+    fun setupAnim() {
+        binding.animationView.speed=0.95F
+        binding.animationView.setMinProgress(0.18F)
+        binding.animationView.playAnimation()
     }
 }
