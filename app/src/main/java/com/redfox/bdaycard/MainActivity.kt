6e8dd332fe4b701dd775c24e3076f025739d5b71
Madhur Mehta <12madhurmehta@gmail.com>
2021-09-11 22:57:59 +0530

    Initial Commit

--- app/src/main/java/com/redfox/bdaycard/MainActivity.kt
+++ app/src/main/java/com/redfox/bdaycard/MainActivity.kt
@@ -2,10 +2,15 @@ package com.redfox.bdaycard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.redfox.bdaycard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+
     }
 }
