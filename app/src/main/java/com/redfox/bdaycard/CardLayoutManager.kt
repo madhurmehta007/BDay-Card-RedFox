f9d9ff0d2e5e1feaa599d201c7e0587027167d86
Madhur Mehta <12madhurmehta@gmail.com>
2021-09-12 12:41:26 +0530

    Added New Activity UI Changes

---
+++ app/src/main/java/com/redfox/bdaycard/CardLayoutManager.kt
@@ -0,0 +1,11 @@
+package com.redfox.bdaycard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardLayoutManager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_layout_manager)
+    }
+}
