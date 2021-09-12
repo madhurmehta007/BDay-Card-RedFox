26ea721479934e804bade5bfbac82b5624e42223
debz-g <debz.exe@gmail.com>
2021-09-13 00:36:53 +0530

    - Added layouts

--- app/src/main/java/com/redfox/bdaycard/CardLayoutManager.kt
+++ app/src/main/java/com/redfox/bdaycard/CardLayoutManager.kt
@@ -2,10 +2,13 @@ package com.redfox.bdaycard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.MobileAds
 
 class CardLayoutManager : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_layout_manager)
+
+
     }
 }
